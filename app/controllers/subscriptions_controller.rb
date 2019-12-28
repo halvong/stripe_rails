9d9aa8c4fc451f8fa5fed9327fadada01b0e558b
Hal Vong <halvong@yahoo.com>
2019-12-27 19:44:56 -0800

    Subscription chp5 done.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -16,18 +16,53 @@ class SubscriptionsController < ApplicationController
       )
 
       current_user.assign_attributes(stripe_subscription_id: subscription.id)
-      current_user.save
+      current_user.assign_attributes(
+          card_brand: params[:card_brand],
+          card_last4: params[:card_last4],
+          card_exp_month: params[:card_exp_month],
+          card_exp_year: params[:card_exp_year],
+          ) if params[:card_last4]
 
+      current_user.save
       flash.notice = "Thanks for subscription!"
       redirect_to root_path
 
     rescue Stripe::CardError => e
-
         flash.alert = e.message
         render action: :new
-
     end
 
   end
 
-end
+  def show
+  end
+
+  def update
+    customer = current_user.stripe_customer
+
+    begin
+
+      source = customer.sources.create(source: params[:stripeToken])
+
+      customer.default_source = source.id
+      customer.save
+
+      current_user.assign_attributes(
+          card_brand: params[:card_brand],
+          card_last4: params[:card_last4],
+          card_exp_month: params[:card_exp_month],
+          card_exp_year: params[:card_exp_year]
+      )
+
+      current_user.save
+      flash.notice = "Your card was updated successfully"
+      redirect_to root_path
+
+    rescue Stripe::CardError => e
+      flash.alert = e.message
+      render action: :new
+    end
+
+  end
+
+end #class
